eda8bd72e1316b1d9b1c2407f860847c5ac5b3a7
Ivan Quirino <ivanquirino1928@gmail.com>
2023-09-15 16:44:27 -0300

    add jest

---
+++ src/app/games/tetris-like/game.test.ts
@@ -0,0 +1,11 @@
+import { rotateClockwise, pieceI, pieceL } from "./game";
+
+describe("rotation/rotateClockwise", () => {
+  test("findPivot", () => {
+    const rotated = rotateClockwise(pieceI, 2);
+
+    
+
+    // expect(rotated).toEqual({ x: 5, y: 1 });
+  });
+});
