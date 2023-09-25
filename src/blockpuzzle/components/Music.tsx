b75ed6eba3d099836c8e0e24b53b208fe9cee302
Ivan Quirino <ivanquirino1928@gmail.com>
2023-09-25 15:08:13 -0300

    play soumusic without re-rendering component

--- src/blockpuzzle/components/Music.tsx
+++ src/blockpuzzle/components/Music.tsx
@@ -1,29 +1,32 @@
 import { useEffect, useRef } from "react";
-import { useGameStore } from "./GameClient";
+import { store } from "./GameClient";
 
 const Music = () => {
-  const status = useGameStore((state) => state.status);
-  const isMusicEnabled = useGameStore((state) => state.settings.music);
   const musicRef = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
     const music = musicRef.current;
+    if (music) music.volume = 0.3;
 
-    if (isMusicEnabled && music) {
-      music.volume = 0.3;
+    const unsub = store.subscribe((state, prevState) => {
+      if (state.settings.music && state.status !== prevState.status) {
+        if (music) {
+          if (state.status === "started") {
+            music.play();
+          }
+          if (state.status === "paused") {
+            music.pause();
+          }
+          if (state.status === "gameover") {
+            music.pause();
+            music.currentTime = 0;
+          }
+        }
+      }
+    });
 
-      if (status === "started") {
-        music.play();
-      }
-      if (status === "paused") {
-        music.pause();
-      }
-      if (status === "gameover") {
-        music.pause();
-        music.currentTime = 0;
-      }
-    }
-  }, [status, isMusicEnabled]);
+    return unsub;
+  }, []);
 
   return (
     <audio ref={musicRef} src="korobeiniki.ogg" loop>
