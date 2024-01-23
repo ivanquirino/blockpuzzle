e1f44c2bcdad582344813c195ef21b29e6f81deb
Ivan Quirino <ivanquirino1928@gmail.com>
2024-01-22 22:31:55 -0300

    correctly mount ssh keys

--- create-dev-container.sh
+++ create-dev-container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-podman run --name blockpuzzle-dev -ti -p 3000:3000 \
+podman secret create pvkey ~/.ssh/id_ed25519
+podman secret create pubkey ~/.ssh/id_ed25519.pub
+
+podman run --name blockpuzzle-dev -d -p 3000:3000 \
     --replace \
-    -v ~/.ssh:/root/.ssh:z \
+    --secret pvkey,target=/root/.ssh/id_ed25519,mode=0600 \
+    --secret pubkey,target=/root/.ssh/id_ed25519.pub \
     blockpuzzle-dev
